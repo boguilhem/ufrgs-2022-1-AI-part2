cd1de76975b0d5c45056da333eaaf3d987338a67
Bruno Guilhem <boguilhem@gmail.com>
2022-08-12 20:05:19 -0300

    feat: minimax pruning algorithm

--- advsearch/your_agent/agent.py
+++ advsearch/your_agent/agent.py
@@ -1,6 +1,7 @@
 import random
 import math
-import copy
+from copy import deepcopy
+from typing import Tuple
 from ..othello import board
 
 # Voce pode criar funcoes auxiliares neste arquivo
@@ -21,21 +22,96 @@ def make_move(the_board, color):
     # a primeira jogada com as pretas.
     # Remova-o e coloque a sua implementacao da poda alpha-beta
     othello_state = OthelloState(the_board, color)
-    return othello_state.random_move()
+    # print("RETORNO DO MAKE MOVE: ", othello_state.minimax_strategy(color, the_board))
+    return othello_state.minimax_strategy(color, the_board)
+    # return othello_state.random_move(color)
 
 
 class OthelloState:
-    def __init__(self, the_board: board, agent_color: str):
-        self.the_board = the_board
+    def __init__(self, board: board, agent_color: str):
+        self.board = board
         self.agent_color = agent_color
         self.rival_color = "W" if agent_color == "B" else "B"
 
-    def valid_moves(self):
-        valid_moves = self.the_board.legal_moves(self.agent_color)
-        return valid_moves
-
-    def random_move(self):
-        if len(self.valid_moves()) > 0:
-            return random.choice(self.valid_moves())
+    def random_move(self, color: str):
+        virtual_board = deepcopy(self.board)
+        valid_moves = virtual_board.legal_moves(color)
+        if len(valid_moves) > 0:
+            return random.choice(valid_moves)
         else:
             return (-1, -1)
+
+    def minimax_strategy(self, color, board) -> Tuple[int, int]:
+        valid_moves = board.legal_moves(color)
+
+        if len(valid_moves) == 0:
+            # print("LEN DE MOVES É ZERO")
+            return (-1, -1)
+
+        agent_movement = random.choice(valid_moves)
+        # print("AGENT_MOVEMENT: ", agent_movement)
+
+        alpha = -math.inf
+        beta = math.inf
+
+        for move in valid_moves:
+            # virtual_board = deepcopy(board)
+            # virtual_board.process_move(move, self.agent_color)
+            # evaluation = self._min_score(test_board, self.opponent_color, -INFINITY, INFINITY, self.depth_limit - 1)
+            evaluation = self.minimax_alpha_beta(board, 2, alpha, beta, color)
+
+            if evaluation > alpha:
+                alpha = evaluation
+                agent_movement = move
+        # print("MOVIMENTO FINAL: ", agent_movement)
+        print("ALPHA: ", alpha)
+        return agent_movement
+
+    def minimax_alpha_beta(self, board: board, depth: int, alpha, beta, color: str):
+        valid_moves = board.legal_moves(color)
+        # print("MOVIMENTOS VALIDOS: ", valid_moves)
+
+        if depth == 0 or board.is_terminal_state():
+            # print("DEPTH É ZERO")
+            return self.heuristics_eval(board, color)
+
+        if color == self.agent_color:
+            # print("ENTROU NO MAX")
+            max_eval = -math.inf
+            for move in valid_moves:
+                virtual_board = deepcopy(board)
+                virtual_board.process_move(move, color)
+                evaluation = self.minimax_alpha_beta(virtual_board, depth - 1, alpha, beta, self.rival_color)
+                max_eval = max(max_eval, evaluation)
+                alpha = max(alpha, evaluation)
+                if beta <= alpha:
+                    break
+            # print("MAX_EVAL: ", max_eval)
+            return max_eval
+
+        else:
+            # print("ENTROU NO MIN")
+            min_eval = math.inf
+            for move in valid_moves:
+                virtual_board = deepcopy(board)
+                virtual_board.process_move(move, color)
+                evaluation = self.minimax_alpha_beta(virtual_board, depth - 1, alpha, beta, self.agent_color)
+                min_eval = min(min_eval, evaluation)
+                beta = min(beta, evaluation)
+                if beta <= alpha:
+                    break
+            # print("MIN_EVAL: ", min_eval)
+            return min_eval
+
+    def heuristics_eval(self, board, color):
+        piece_score_heuristic = self.piece_score(board, color)
+        final_heuristic = piece_score_heuristic
+        # print("HEURISTICS EVAL: ", final_heuristic)
+        return final_heuristic
+
+    def piece_score(self, board: board, color: str):
+        rival_pieces = board.num_pieces(self.rival_color)
+        agent_pieces = board.num_pieces(color)
+
+        score = agent_pieces - rival_pieces
+        return score
