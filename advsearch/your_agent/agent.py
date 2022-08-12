25b2e7352a1b9a77b5ff985517119361d0783447
Leonardo Abreu <abreu.leo@hotmail.com>
2022-08-12 18:54:58 -0300

    feat: created heuristic function

--- advsearch/your_agent/agent.py
+++ advsearch/your_agent/agent.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 # Voce pode criar funcoes auxiliares neste arquivo
 # e tambem modulos auxiliares neste pacote.
@@ -18,5 +17,27 @@ def make_move(the_board, color):
     # o codigo abaixo apenas retorna um movimento aleatorio valido para
     # a primeira jogada com as pretas.
     # Remova-o e coloque a sua implementacao da poda alpha-beta
+
+    print(heuristic(the_board, color))
     return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
 
+
+def heuristic(the_board, color):
+    """
+    Retorna o numero de jogadas possiveis do oponente
+    :return: int
+    """
+    # inicia em zero o numero de jogadas possiveis
+    opponent_moves = 0
+
+    # verifica qual a cor do jogador para pegar a do oponente
+    opponent_color = 'B' if color == 'W' else 'W'
+
+    # percorre o board buscando posicoes validas
+    tiles = [(x, y) for x in range(8) for y in range(8)]
+    for x, y in tiles:
+        if(the_board.is_legal((x, y), opponent_color)):
+            opponent_moves += 1
+
+    # retorna o numero de jogadas possiveis
+    return opponent_moves
